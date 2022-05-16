950362a49187e5e02780def8a1fa8f4fee56a3a6
Davis Shoup <davisshoup@yahoo.com>
2022-05-16 18:00:58 -0400

    2nd commit

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.listen(3000, () => {
     console.log("listening!");
 });
 
-app.get('/greeting/:name', (req, res) => {
+app.get('/greeting', (req, res) => {
     res.send('Hello, stranger.');
+});
+
+app.get('/greeting/:name', (req, res) => {
+    res.send("Hello there, " + req.params.name);
 });
