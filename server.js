91281502f00ca119bc3e88924e37d059ee1f1051
Davis Shoup <davisshoup@yahoo.com>
2022-05-16 18:22:10 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.get('/greeting', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.send("Hello there, " + req.params.name);
 });
+
+app.get("/tip/:total/:tipPrecentage", (req, res) => {
+    console.log(req.params)
+    res.send("Your calculated tip: " + ( .01 * req.params.tipPrecentage) * req.params.total );
+  })
